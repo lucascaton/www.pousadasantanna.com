27d2926363ade165e0ae745acaf0f4d12c8a50e3
Lucas Catón <lucascaton@gmail.com>
2011-02-04 08:38:33 -0200

    Cacheando páginas estáticas

--- app/controllers/static_content_controller.rb
+++ app/controllers/static_content_controller.rb
@@ -1,5 +1,7 @@
 class StaticContentController < ApplicationController
 
+  caches_page :index, :accommodation, :gallery, :localization, :contact
+
   def index; end
   def accommodation; end
   def gallery; end
