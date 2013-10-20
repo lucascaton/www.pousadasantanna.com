1bebb43d966f0019cb2b7fe99e876cb209c002eb
Lucas Caton <lucascaton@gmail.com>
2013-10-20 13:13:39 +1000

    Fixing carosel bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,7 @@
 //= require_tree .
 
 $(function(){
-  $('a[href*=#]:not([href=#])').click(function(){
+  $('a.scroll[href*=#]:not([href=#])').click(function(){
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
